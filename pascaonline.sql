3f210ee15cb2b11e07f02faa3b3263b61527e179
Bagus Ramadhan <bagdhan@gmail.com>
2019-01-22 08:46:25 +0700

    Update pascaonline.sql
    update

--- pascaonline.sql
+++ pascaonline.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2019 at 10:46 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 5.6.39
+-- Waktu pembuatan: 22 Jan 2019 pada 02.39
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 5.6.39
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bimbingan`
+-- Struktur dari tabel `akses`
+--
+
+CREATE TABLE `akses` (
+  `id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `akses`
+--
+
+INSERT INTO `akses` (`id`, `status`) VALUES
+(1, 'admin'),
+(2, 'mahasiswa'),
+(3, 'dosen');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `bimbingan`
 --
 
 CREATE TABLE `bimbingan` (
@@ -39,7 +59,7 @@ CREATE TABLE `bimbingan` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dosen_pembimbing`
+-- Struktur dari tabel `dosen_pembimbing`
 --
 
 CREATE TABLE `dosen_pembimbing` (
@@ -53,7 +73,7 @@ CREATE TABLE `dosen_pembimbing` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kelompok`
+-- Struktur dari tabel `kelompok`
 --
 
 CREATE TABLE `kelompok` (
@@ -66,7 +86,7 @@ CREATE TABLE `kelompok` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mahasiswa`
+-- Struktur dari tabel `mahasiswa`
 --
 
 CREATE TABLE `mahasiswa` (
@@ -75,14 +95,49 @@ CREATE TABLE `mahasiswa` (
   `nim` varchar(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `kodemayor` varchar(5) NOT NULL,
-  `strata` varchar(2) NOT NULL,
+  `namamayor` varchar(255) NOT NULL,
+  `programstudi` varchar(255) NOT NULL,
   `status_verifikasi` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `moderator`
+-- Struktur dari tabel `mayor`
+--
+
+CREATE TABLE `mayor` (
+  `kodemayor` varchar(5) NOT NULL,
+  `namamayor` varchar(255) NOT NULL,
+  `strata` varchar(2) NOT NULL,
+  `prefix` varchar(3) NOT NULL,
+  `kodenasional` varchar(10) NOT NULL,
+  `gelar` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `migration`
+--
+
+CREATE TABLE `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1548041735),
+('m130524_201442_init', 1548041741);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `moderator`
 --
 
 CREATE TABLE `moderator` (
@@ -94,7 +149,7 @@ CREATE TABLE `moderator` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orang`
+-- Struktur dari tabel `orang`
 --
 
 CREATE TABLE `orang` (
@@ -113,10 +168,24 @@ CREATE TABLE `orang` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pps500`
+-- Struktur dari tabel `pps500_nilai`
 --
 
-CREATE TABLE `pps500` (
+CREATE TABLE `pps500_nilai` (
+  `id` int(11) NOT NULL,
+  `nim` varchar(50) NOT NULL,
+  `nilai` varchar(50) NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `histori` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pps500_paket`
+--
+
+CREATE TABLE `pps500_paket` (
   `id` int(11) NOT NULL,
   `nama` varchar(200) NOT NULL,
   `keterangan` text NOT NULL,
@@ -128,7 +197,7 @@ CREATE TABLE `pps500` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pps500_sesi`
+-- Struktur dari tabel `pps500_sesi`
 --
 
 CREATE TABLE `pps500_sesi` (
@@ -137,7 +206,7 @@ CREATE TABLE `pps500_sesi` (
   `tempat` varchar(100) NOT NULL,
   `hari` varchar(50) NOT NULL,
   `waktu` varchar(50) NOT NULL,
-  `ruangan` varchar(30) NOT NULL,
+  `ruang` varchar(30) NOT NULL,
   `kuota` int(4) NOT NULL,
   `aktif` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -145,7 +214,7 @@ CREATE TABLE `pps500_sesi` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `seminar`
+-- Struktur dari tabel `seminar`
 --
 
 CREATE TABLE `seminar` (
@@ -160,7 +229,7 @@ CREATE TABLE `seminar` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `seminar_sesi`
+-- Struktur dari tabel `seminar_sesi`
 --
 
 CREATE TABLE `seminar_sesi` (
@@ -170,27 +239,138 @@ CREATE TABLE `seminar_sesi` (
   `aktif` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `akses_id` int(11) DEFAULT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `akses_id`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'admin', 'AqjuWBktYQYE81fHTpdbQ7gjVVCvzvQ_', '$2y$13$JYx2oDTADoK2dvWAwZtryOPxWsivIfuGR7SzcjCEL9UUbwpT.t6o6', 'yiIqcBUMYAskiIjilQsuhpbywjLcxRhW_1548043206', 'alay.doski@gmail.com', 0, 10, 1548042196, 1548043206);
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `kelompok`
+-- Indeks untuk tabel `akses`
+--
+ALTER TABLE `akses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `bimbingan`
+--
+ALTER TABLE `bimbingan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `dosen_pembimbing`
+--
+ALTER TABLE `dosen_pembimbing`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `kelompok`
 --
 ALTER TABLE `kelompok`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `mahasiswa`
+-- Indeks untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `seminar_sesi`
+-- Indeks untuk tabel `mayor`
+--
+ALTER TABLE `mayor`
+  ADD PRIMARY KEY (`kodemayor`);
+
+--
+-- Indeks untuk tabel `migration`
+--
+ALTER TABLE `migration`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indeks untuk tabel `moderator`
+--
+ALTER TABLE `moderator`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `orang`
+--
+ALTER TABLE `orang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pps500_nilai`
+--
+ALTER TABLE `pps500_nilai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pps500_paket`
+--
+ALTER TABLE `pps500_paket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pps500_sesi`
+--
+ALTER TABLE `pps500_sesi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `seminar`
+--
+ALTER TABLE `seminar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `seminar_sesi`
 --
 ALTER TABLE `seminar_sesi`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
